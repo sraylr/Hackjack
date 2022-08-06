7d2636d0e0aba205213ea53dd9d68141bf816883
sraylr <ogjkerr@gmail.com>
2022-08-05 17:24:05 -0700

    make it compile

--- packages/hardhat/contracts/Hackjack.sol
+++ packages/hardhat/contracts/Hackjack.sol
@@ -9,6 +9,8 @@ contract Hackjack is ERC721 {
 
     uint256 public counterIDs = 0; // assign each game an index
 
+    uint256 public salt = 314159265;
+
     event NewGame(uint256 gameId, address player, uint256 bet, uint8 firstCard, uint8 secondCard, uint8 dealerCard);
     event PlayerHit(uint256 gameId, address player, uint8 card);
     event DealerHit(uint256 gameId, uint8 card);
@@ -16,8 +18,8 @@ contract Hackjack is ERC721 {
     event Winner(uint256 gameId, address player, uint256 value);
 
     struct Hand {
-        uint8[] playerCards;
-        uint8[] dealerCards;
+        uint8 playerCardValue;
+        uint8 dealerCardValue;
         uint256 bet;
         bool winner;
         bool busted;
@@ -27,17 +29,19 @@ contract Hackjack is ERC721 {
 
     mapping(uint256 => Hand) public games; // mapping from ID to Hand struct
 
-    modifier playable(_gameId) {
+    modifier playable(uint256 _gameId) {
         require(games[_gameId].busted == false, "busted");
         require(games[_gameId].winner == false, "winner");
+        _;
     }
 
-    modifier onlyOwner(_gameId) {
+    modifier onlyOwner(uint256 _gameId) {
         require(ownerOf(_gameId) == msg.sender, "not owner");
+        _;
     }
 
-    constructor(string memory _uri) ERC721(_uri) {
-        cardValues[0] = 1; // or 11?? not sure how to do this
+    constructor() ERC721("Hackjack Hand", "JACK") {
+        cardValues[0] = 11;
         cardValues[1] = 2;
         cardValues[2] = 3;
         cardValues[3] = 4;
@@ -55,65 +59,69 @@ contract Hackjack is ERC721 {
     function newGame() public payable {
         require(msg.value >= minBet && msg.value <= maxBet, "Invalid bet amount"); // Enforce bet limits
         require(address(this).balance >= msg.value * 2); // Ensure contract has enough funds to pay // This could permanently kill the contract if balance ever drops below minBet * 2
-        // TODO: need to mark bet winning value as unavailable in the contract to ensure funds are available for withdrawl in the case of a winning hand
-        _gameId = counterIDs;
-        counterIDs++;
-        games[_gameId] = Hand([], [], msg.value, false, false); // create Hand
-        _mint(msg.sender, _gameId); // create NFT representing ownership of hand
-        
-        dealDealer(_gameId);
-        dealPlayer(_gameId);
-        dealPlayer(_gameId);
 
-        emit NewGame(_gameId, msg.sender, games[_gameId].bet, games[_gameId].playerCards[0], games[_gameId].playerCards[1], games[_gameId].dealerCards[0]);
+        // TODO: need to mark bet winning value as unavailable in the contract to ensure funds are available for withdrawl in the case of a winning hand
+
+        uint256 _gameId = counterIDs;
+        counterIDs++;
+
+        games[_gameId] = Hand(0, 0, msg.value, false, false); // create Hand
+        
+        uint8 playerCard1 = dealDealer(_gameId);
+        uint8 playerCard2 = dealPlayer(_gameId);
+        uint8 dealerCard = dealPlayer(_gameId);
+
+        _mint(msg.sender, _gameId); // create NFT representing ownership of hand
+
+        emit NewGame(_gameId, msg.sender, games[_gameId].bet, playerCard1, playerCard2, dealerCard);
     }
 
-    function dealPlayer(uint256 _gameId) internal {
-        rand = getRandomNumber() % 13;
+    function dealPlayer(uint256 _gameId) internal returns(uint8 card) {
+        uint8 rand = uint8(getRandomNumber() % 13);
         card = cardValues[rand];
-        games[_gameId].playerCards.push(card);
-        check(_gameId);
+        games[_gameId].playerCardValue += card;
 
         emit PlayerHit(_gameId, msg.sender, card);
+
+        return card;
     }
 
-    function dealDealer(uint256 _gameId) internal {
-        rand = getRandomNumber() % 13;
+    function dealDealer(uint256 _gameId) internal returns(uint8 card) {
+        uint8 rand = uint8(getRandomNumber() % 13);
         card = cardValues[rand];
-        games[_gameId].dealerCards.push(card);
+        games[_gameId].dealerCardValue += card;
 
         emit DealerHit(_gameId, card);
+
+        return card;
     }
 
-    function hit(_gameId) public playable onlyOwner {
+    function hit(uint256 _gameId) public playable(_gameId) onlyOwner(_gameId) {
         dealPlayer(_gameId);
         check(_gameId);
     }
 
-    function stand(_gameId) public playable onlyOwner {
+    function stand(uint256 _gameId) public playable(_gameId) onlyOwner(_gameId) {
         resolve(_gameId);
     }
 
-    function check(_gameId) internal view {
-        cardTotal = getPlayerTotal(_gameId);
-
-        if(cardTotal > 21) {
+    function check(uint256 _gameId) internal {
+        if(games[_gameId].playerCardValue > 21) {
             games[_gameId].busted = true;
         }
-        else if(cardTotal == 21) {
+        else if(games[_gameId].playerCardValue == 21) {
             games[_gameId].winner = true;
         }
     }
 
-    function resolve(_gameId) internal {
+    function resolve(uint256 _gameId) internal {
 
         uint8 playerTotal;
         uint8 dealerTotal;
 
-        // while dealerTotal is less than 17, deal another card and update total
-        while(dealerTotal <= 16) {
+        // while dealerTotal is <17, deal another card and update total
+        while(games[_gameId].dealerCardValue < 17) {
             dealDealer(_gameId);
-            dealerTotal = getDealerTotal(_gameId);
         }
 
         // check to see who won
@@ -131,39 +139,26 @@ contract Hackjack is ERC721 {
         // end game
         if(games[_gameId].busted) {
             // If busted, burn
-            _burn(msg.sender, _gameId); 
+            _burn(_gameId); 
             emit Busted(_gameId, msg.sender);
         }
         else if(games[_gameId].winner) {
             // If winner, burn, THEN payout 2x
-            leftToPay = games[_gameId].bet * 2;
-            _burn(msg.sender, _gameId);
+            uint256 leftToPay = games[_gameId].bet * 2;
+            _burn(_gameId);
             (bool success, ) = address(payable(msg.sender)).call{ value: leftToPay }("");
             require(success, "failed"); 
 
             //mint reward nft
+            
             emit Winner(_gameId, msg.sender, leftToPay);
         }
     }
 
-    function getRandomNumber() internal {
-        // chainlink does some magic
-        return randomNumber;
+    function getRandomNumber() internal returns(uint256) {
+        // chainlink could do some magic
+        uint256 blockNumber = block.number;
+        salt++;
+        return uint256(keccak256(abi.encodePacked(blockNumber + salt)));
     }  
-
-    function getDealerTotal(uint256 _gameId) public view {
-        uint256 total = 0;
-        for(uint256 i = 0; i < games[_gameId].dealerCards.length; i++) {
-            total = total + games[_gameId].dealerCards[i];
-        }
-        return total;
-    }
-
-    function getPlayerTotal(uint256 _gameId) public view {
-        uint256 total = 0;
-        for(uint256 i = 0; i < games[_gameId].playerCards.length; i++) {
-            total = total + games[_gameId].playerCards[i];
-        }
-        return total;
-    }
 }
